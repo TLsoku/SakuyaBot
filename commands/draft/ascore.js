cae8a74dd14cb709c1212ab7177d6ab32c43cfb9
BensonLai <biingchyn.benson.lai@gmail.com>
2021-06-22 10:52:49 -0400

    Ascore implementation

--- commands/draft/ascore.js
+++ commands/draft/ascore.js
@@ -1,14 +1,23 @@
+const fetch = require("node-fetch");
+
 module.exports = {
 	name: 'ascore',
 	description: 'return anime score from MAL',
 	args: true,
-	execute(message, args) {
-		if(!args==0){
+	execute: async (message, args) => {
+		
+		if(args[0]==0 || !args[0]){
 			message.channel.send('No anime ID');
 			return;
 		}
+		
 		message.channel.send(`Anime ID: ${args[0]}`);
+		var URL = "https://api.jikan.moe/v3/anime/" + args[0];
+		message.channel.send(URL);
+		const { file } = await fetch(URL).then(res => res.json()).then(json => console.log(json));
+		//.then(response => response.json());
+		message.channel.send(file.score);
+        
 	},
-	
 
 };
