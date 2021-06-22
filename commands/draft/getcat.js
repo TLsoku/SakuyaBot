52b6ae7ca1e4755fbec1a7887f5f3187d62b6ba6
BensonLai <biingchyn.benson.lai@gmail.com>
2021-06-22 00:11:58 -0400

    nuked some stuff cuz broke

--- commands/draft/getcat.js
+++ commands/draft/getcat.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'post cat',
 	execute: async (message, args) => {
 		message.channel.send('getcat');
-
+		//Used node fetch
         const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 		message.channel.send(file);
         
