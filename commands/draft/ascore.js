c5b05cf5f0ace710aeeb41deab96d65449d2301a
BensonLai <biingchyn.benson.lai@gmail.com>
2021-06-24 13:54:43 -0400

    Animescore done

--- commands/draft/ascore.js
+++ commands/draft/ascore.js
@@ -10,14 +10,17 @@ module.exports = {
 			message.channel.send('No anime ID');
 			return;
 		}
-		
+
 		message.channel.send(`Anime ID: ${args[0]}`);
 		var URL = "https://api.jikan.moe/v3/anime/" + args[0];
 		message.channel.send(URL);
-		const file = await fetch(URL).then(res => res.json()).then(json => console.log(json));
-		//.then(response => response.json());
-		console.log(file[score]);
-		//message.channel.send(file[score]);
+		const jsonIn = await fetch(URL).then(res => res.json()).then(data => { 
+			console.log(data)
+			console.log(data.score);
+			message.channel.send("The anime score: " + data.score);
+		 })
+		 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
         
 	},
 
