01e3a7e77ed441d19fac49f569413cbf1fffa523
BensonLai <biingchyn.benson.lai@gmail.com>
2021-07-12 22:22:21 -0400

    pull
    think pull request blew something up

---
+++ schemas/animedraft-schema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userIDString = {
+    type: String,
+    required: true,
+    primaryKey: true
+}
+
+const animedraftSchema = mongoose.Schema({
+    userID: userIDString,
+    anime1: String,
+    anime2: String,
+    anime3: String,
+    anime4: String,
+    anime5: String,
+    anime6: String,
+    antipick1: String,
+    antipick2: String    
+})
+
+module.exports = mongoose.model("animedraft", animedraftSchema);
